704250e82b469924093d1bc26c1499d5330a3983
renaranj <155959338+renaranj@users.noreply.github.com>
2025-06-15 22:14:08 +0200

    Create sqlite-test.py

---
+++ sqlite-test.py
@@ -0,0 +1,16 @@
+import sqlite3
+conn = sqlite3.connect('test.db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INT PRIMARY KEY,
+   fname TEXT,
+   lname TEXT,
+   gender TEXT);
+""")
+conn.commit()
+cur.execute("""INSERT INTO users(userid, fname, lname, gender) 
+   VALUES('00001', 'Nik', 'Piepenbreier', 'male');""")
+conn.commit()
+cur.execute("SELECT * FROM users;")
+all_results = cur.fetchall()
+print(all_results)
