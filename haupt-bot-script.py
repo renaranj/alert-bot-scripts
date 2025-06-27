ceaced31ca68c1b81afd28a19728042a42ecb449
renaranj <155959338+renaranj@users.noreply.github.com>
2025-06-28 00:50:27 +0200

    Update haupt-bot-script.py

--- haupt-bot-script.py
+++ haupt-bot-script.py
@@ -562,6 +562,10 @@ def alarm_ichimoku_crosses(symbol, candles, interval="4h", priority=False, debug
             print(f"Tenkan/Kijun previous:({tenkan.iloc[-2]:.4f},{kijun.iloc[-2]:.4f}), current:({tenkan.iloc[-1]:.4f},{kijun.iloc[-1]:.4f})")
         send_telegram_alert(symbol, combined_msg, interval, priority)
                        
+def send_telegram_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": chat_id, "text": text})
+        
 def send_telegram_alert(symbol, message, interval="4h", priority=False):
     if "_" in symbol:
         symbol = symbol.replace("_USDT", "USDT.P")
